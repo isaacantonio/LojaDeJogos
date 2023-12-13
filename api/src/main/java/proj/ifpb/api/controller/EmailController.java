83c0bad700e2db5b0cae776ec771ad84b96db593
Isaac Antônio <isaacasb16@gmail.com>
2023-12-13 11:08:52 -0300

    ADD authentication email

--- api/src/main/java/proj/ifpb/api/controller/EmailController.java
+++ api/src/main/java/proj/ifpb/api/controller/EmailController.java
@@ -6,6 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
